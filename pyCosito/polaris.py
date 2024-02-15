5653bed88855ef10857cbb830eaced7aae5bb4f7
RayHE <rayhe88@gmail.com>
2024-02-15 11:59:23 -0600

    File polaris with mapping the nodes with fworker and add the binary executable

---
+++ pyCosito/polaris.py
@@ -0,0 +1,44 @@
+# Map the FW-task on polaris single-queue allocation
+
+""" Polaris Mapping """
+
+import os
+from fireworks.core.fworker import FWorker
+
+def createCommand(node, software):
+    binary = os.environ.get("EXE")
+    if binary is None:
+        binary = '/lus/eagle/projects/catalysis_aesp/abagusetty/qe-7.1/build_cuda_nompiaware/bin/pw.x'
+
+    if software == 'Espresso':
+        command = 'mpiexec --hosts {} -n 4 --ppn 4 --depth=8 --cpu-bind depth --env OMP_NUM_THREADS=8 --env CUDA_VISIBLE_DEVICES=0,1,2,3 {} -in PREFIX.pwi > PREFIX.pwo'.format(node, binary)
+    elif software == 'PWDFT':
+        gpu_tile = '/lus/gila/projects/catalysis_aesp_CNDA/raymundohe/bin/gpu_tile_compact.sh'
+        command = 'mpiexec --hosts {} -n 12 --ppn 12 --env OMP_NUM_THREADS=1 {} {} PREFIX.nwxi > PREFIX.nwxo'.format(node,gpu_tile, binary)
+#add NWChem?
+    return command
+
+
+def createFWorkers(num_jobs):
+
+    nodes_list = []
+    nodefile = os.environ.get("PBS_NODEFILE")
+
+    if nodefile is None:
+        for i in range(num_jobs):
+            nodes_list.append(f'localhost_{i}')
+    else:
+        with open(nodefile) as f:
+            for line in f:
+                nodes_list.append(line.strip('\n'))
+
+    fworkers = []
+
+    for i in range(num_jobs):
+        host = nodes_list[i]
+        env = {'host': host}
+
+        fworkers.append(FWorker(env=env))
+
+    return fworkers
+
